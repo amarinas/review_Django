e11b578d4db49d2603f02156c3cfaf9ae69b8693
amarinas <ali.marinas@icloud.com>
2017-05-26 16:29:07 -0700

    added more review items

---
+++ django/full_stack_books/apps/full_stack/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from .models import Book
+# Create your views here.
+def index(request):
+
+    context = {
+    "books": Book.objects.all()
+    }
+    return render(request, 'full_stack/index.html', context)
+
+def create(request):
+    Book.objects.create(title=request.POST['title'], author=request.POST['author'], category=request.POST['category'])
+
+    return redirect('/')
