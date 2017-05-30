010053ac716f787f2d750a04366ef37491a94d0a
amarinas <ali.marinas@icloud.com>
2017-05-29 20:38:07 -0700

    update

--- django/appointments/apps/appointment/models.py
+++ django/appointments/apps/appointment/models.py
@@ -2,9 +2,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+class ScheduleManager(models.Manager):
+
+    def make_appointment(self, *args):
+        errormsg =[]
+        status = True
+        if args[0] < 0:
+            error.msg.append('date cannot be empty')
+            status = False
+        else:
+            Schedule.objects.create(date= args[0], time=args[1])
+
+
+
+
 # Create your models here.
 class Schedule(models.Model):
     date = models.DateField(auto_now=False, auto_now_add=False)
     time = models.DateTimeField(auto_now=False, auto_now_add=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ScheduleManager()
--- django/appointments/apps/appointment/views.py
+++ django/appointments/apps/appointment/views.py
@@ -1,14 +1,29 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Schedule
 
 # Create your views here.
 def index(request):
+
+
     return render(request, 'appointment/index.html')
 
 def create(request):
     print request.POST
+    if request.method == "GET":
+        return redirect('/')
+    appointment_check = Schedule.objects.make_appointment(request.POST['date'], request.POST['time'])
+    if 'error' in appointment_check:
+        error = make_appointment['error']
+        for msg in error:
+            messages.error(request, msg)
+        return redirect('/')
+
+
 
     return redirect('/success')
 
 def success(request):
-    return render(request, 'appointment/success.html')
+    context={
+    'appointments': Schedule.objects.all()
+    }
+    return render(request, 'appointment/success.html', context)
